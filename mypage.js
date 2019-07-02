b7e2f2954db18ad46c6a66fd73b31c2dd83df2a5
dishahauzaree <44508771+dishahauzaree@users.noreply.github.com>
2019-07-02 15:17:34 -0700

    Add files via upload

---
+++ mypage.js
@@ -0,0 +1,13 @@
+  window.alert("Disha is super cool");
+
+  button.collapsible {
+    background-color: #eee;
+    color: #444;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+  }
